5ea126515e0c56fbc355a29d2fb43336b3a47eba
hoonyhoney <hoonyhoeny@gmail.com>
2024-01-03 22:31:56 +0900

    next.js

---
+++ src/app/components/HomeButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+export default function HomeButton(){
+
+    const router=useRouter()
+    const handleClick = () =>{
+       router.push('/')
+    }
+    
+    return  <button onClick={handleClick}>홈으로2</button>
+}
